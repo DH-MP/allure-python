ee57e17277d1ee73adc8f8fbd8088a36317a9bbd
DH-MP <viet_mike.pham@hotmail.com>
2022-01-09 13:35:35 -0500

    Update utils.py
    Fix for https://github.com/allure-framework/allure-python/issues/636
    
    Copied /Modified pytest_markers and mark_to_str function from allure-python utils

--- allure-pytest-bdd/src/utils.py
+++ allure-pytest-bdd/src/utils.py
@@ -5,6 +5,7 @@ from allure_commons.model2 import StatusDetails
 from allure_commons.model2 import Status
 from allure_commons.model2 import Parameter
 from allure_commons.utils import format_exception
+from allure_commons.utils import represent
 
 
 def get_step_name(node, step):
@@ -49,4 +50,44 @@ def get_pytest_report_status(pytest_report):
 def get_params(node):
     if hasattr(node, 'callspec'):
         params = node.callspec.params
+        if '_pytest_bdd_example' in params:
+            params = params['_pytest_bdd_example']
         return [Parameter(name=name, value=value) for name, value in params.items()]
+
+
+def get_package(node):
+    parts = node.nodeid.split('::')
+    path = parts[0].rsplit('.', 1)[0]
+    return path.replace('/', '.')
+
+
+def pytest_markers(item):
+    for keyword in item.keywords.keys():
+        if any([keyword.startswith('allure_'), keyword == 'parametrize']):
+            continue
+        marker = item.get_closest_marker(keyword)
+        if marker is None:
+            continue
+        mark_str = mark_to_str(marker)
+        if mark_str is None:
+            continue
+        yield mark_str
+
+
+def mark_to_str(marker):
+    args = [represent(arg) for arg in marker.args]
+    kwargs = ['{name}={value}'.format(name=key, value=represent(marker.kwargs[key])) for key in marker.kwargs]
+    skip_markers = ('usefixtures')
+    builtin_pytest_markers = ('filterwarnings', 'skip', 'skipif', 'xfail', 'tryfirst', 'trylast', 'XRAY')
+    if marker.name in skip_markers:
+        return None
+    if marker.name in builtin_pytest_markers:
+        markstr = '@{name}'.format(name=marker.name)
+    else:
+        markstr = '{name}'.format(name=marker.name)
+
+    if args or kwargs:
+        parameters = ', '.join(args + kwargs)
+        markstr = '{}({})'.format(markstr, parameters)
+
+    return markstr
